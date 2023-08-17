4af609ba929b4d852bc65f555798d8289f7b985a
krinishbhuva <krinishbhuva204@gmail.com>
2023-08-17 18:28:19 +0530

    fast

--- js/main.js
+++ js/main.js
@@ -52,9 +52,9 @@
         autoplay: true,
         smartSpeed: 1500,
         items: 1,
-        dots: true,
+        dots: false,
         loop: true,
-        nav : true,
+        nav : false,
         navText : [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
